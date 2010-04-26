a8480de9966dfd45bb37bedba2b9e2b11586d140
Frederik De Bleser <frederik@pandora.be>
2010-04-26 18:16:23 +0200

    Text about travel, fatter letters and random movement.

--- src/main/scala/travelling/FastAttract.scala
+++ src/main/scala/travelling/FastAttract.scala
@@ -1,10 +1,12 @@
 package travelling
 
+import scala.math.random
+
 class FastAttract(override val system: ParticleSystem) extends Behavior(system) {
   var weight = 1.0f
 
   override def apply(p: Particle, dt: Float) {
     val mid = (p.target - p.pos) / 5
-    p.pos += mid
+    p.pos += mid + Vec(random.toFloat * 5f, random.toFloat * 5f)
   }
 }
--- src/main/scala/travelling/ReaderTool.scala
+++ src/main/scala/travelling/ReaderTool.scala
@@ -9,7 +9,17 @@ import scala.collection.mutable.Map
  * Allows you to read a text by morphing the letters.
  */
 class ReaderTool(override val p: ToolContainer) extends Tool(p) {
-  val text = """The Quick Brown Fox Jumps Over The Lazy Dog."""
+  val text = """Travel is the movement of people between relatively distant geographical locations for any purpose and
+  |any duration, with or without any means of transport. Travel also includes relatively short stays between successive
+  |movements. Movements between locations requiring only a few minutes are not considered as travel. As an activity,
+  |"travel" also covers all the activities performed during a travel. Travel is a wider concept than a trip. A round trip
+  |is a particular type of travel whereby a person moves from his/her usual residence to one or several distant locations
+  |and returns. A trip can also be part of a round trip. Travel is most commonly done for recreation or to visit friends
+  |and family (generally as part of tourism), for business or for commuting; but may be for numerous other reasons, such
+  |as health care, migration, fleeing war, etc. Travel may occur by walking or human-powered mode, or through mechanical
+  |vehicles, either as private or public transport. Travel may be local, regional, national (domestic) or international.
+  |In some countries, non-local internal travel may require an internal passport, while international travel typically
+  |requires a passport and visa. """.stripMargin
   val initials = """ABCDEFGHIJKLMNOPQRSTUVWXYZ"""
   val players = Map[String, LetterFlock]()
   val system = new ParticleSystem(800, 600)
@@ -31,9 +41,11 @@ class ReaderTool(override val p: ToolContainer) extends Tool(p) {
   override def draw() {
     system.update(0.1f)
 
-    p.background(90)
+    p.fill(60, 30)
+    p.rect(0, 0, p.width, p.height)
     p.noFill
     p.stroke(255)
+    p.strokeWeight(5f)
 
     p.pushMatrix
     p.scale(0.5f)
